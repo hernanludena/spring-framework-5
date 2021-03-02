40d92f45bb15eb7a93dd691cddfd48990db856d9
hernanludena <hernanludena@yahoo.com>
2021-03-01 20:47:05 -0500

    Clase usuario

--- formularios/3_spring-boot-form[validacion_base_simple]/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ formularios/3_spring-boot-form[validacion_base_simple]/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -20,18 +20,24 @@ public class FormController {
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
 		model.addAttribute("titulo", "Formulario usuarios");
-		model.addAttribute("usuario", usuario);
+		model.addAttribute("usuario", usuario); //objeto por defecto
 		return "form";
 	}
 	
+	//@Valid validaciones java para el objeto Usuario
+	//BindingResult almacena errores de validacion en objeto
 	@PostMapping("/form")
+	//public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model) {/
 	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 		
 		model.addAttribute("titulo", "Resultado form");
 		
 		if(result.hasErrors()) {
 			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err ->{
+			
+			//FieldErrors> lista de errores
+			result.getFieldErrors().forEach(err ->{  //expresion lambda
+				//campo + mensaje
 				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
 			});
 			model.addAttribute("error", errores);
