35936cf0655e75c80dff69edb68f9d980b3df723
hernanludena <hernanludena@yahoo.com>
2021-03-28 20:38:25 -0500

    Spring security

--- spring-security/spring-boot-data-jpa[1_security]/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ spring-security/spring-boot-data-jpa[1_security]/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -18,6 +18,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private LoginSuccessHandler successHandler;
 	
+	//Proteger las rutas
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -29,7 +30,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers("/factura/**").hasAnyRole("ADMIN")
 		.anyRequest().authenticated()
 		.and()
-		    .formLogin()
+		    .formLogin() 
 		        .successHandler(successHandler)
 		        .loginPage("/login")
 		    .permitAll()
@@ -37,7 +38,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		.logout().permitAll()
 		.and()
 		.exceptionHandling().accessDeniedPage("/error_403");
-
+		//implementar formularios de login y logout
 	}
 
 	@Autowired
@@ -48,9 +49,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		 * UserBuilder users = User.withDefaultPasswordEncoder();
 		 * */
 		
+		//Registramos Password y forma de encriptar la contrase;a
 		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode); //java 8, con llamada estatica, simplificacion de lamda
+		//Con expresion lambda
+		//UserBuilder users = User.builder().passwordEncoder(password -> encoder.encode(password));
 		
+		//Configuramos en memoria y creamos 2 usuarios
 		build.inMemoryAuthentication()
 		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
 		.withUser(users.username("andres").password("12345").roles("USER"));
